623a35bcf4c7c5b20025c95d7f641136ca91f339
照屋勇太 <97546104+b047ff@users.noreply.github.com>
2022-07-28 12:16:28 +0900

    Add files via upload

---
+++ myapp/bbs/views.py
@@ -0,0 +1,25 @@
+from django.shortcuts import render
+from django.http import HttpResponse
+from .models import Article
+from django.views import generic
+
+def index(request):
+    return HttpResponse('Hello Django')
+ 
+def detail(request, id):
+    return HttpResponse('detail ' + str(id))
+ 
+def create(request):
+    return HttpResponse('create')
+ 
+def update(request, id):
+    return HttpResponse('update ' + str(id))
+ 
+def delete(request, id):
+    return HttpResponse('delete ' + str(id))
+
+class IndexView(generic.ListView):
+    model = Article
+ 
+class DetailView(generic.DetailView):
+    model = Article
