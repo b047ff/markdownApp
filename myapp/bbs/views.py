a1962d8c984207a5da9cb2a227f7bb31f10efb2c
teruyayuta <teruyayuta@DESKTOP-7LJ7218.localdomain>
2022-07-29 14:54:06 +0900

    チュートリアル終了

--- myapp/bbs/views.py
+++ myapp/bbs/views.py
@@ -3,6 +3,12 @@ from django.http import HttpResponse
 from .models import Article
 from django.views import generic
 
+from django.urls import reverse_lazy
+from django.contrib.auth.mixins import LoginRequiredMixin
+from django.core.exceptions import PermissionDenied
+
+from django.db.models import Q # 追加
+
 def index(request):
     return HttpResponse('Hello Django')
  
@@ -11,15 +17,51 @@ def detail(request, id):
  
 def create(request):
     return HttpResponse('create')
+
+class UpdateView(LoginRequiredMixin, generic.edit.UpdateView):
+    model = Article
+    fields = ['content', 'title', ]
  
-def update(request, id):
-    return HttpResponse('update ' + str(id))
+    def dispatch(self, request, *args, **kwargs):
+        obj = self.get_object()
  
-def delete(request, id):
-    return HttpResponse('delete ' + str(id))
+        if obj.author != self.request.user:
+            raise PermissionDenied('You do not have permission to edit.')
+ 
+        return super(UpdateView, self).dispatch(request, *args, **kwargs)
+
+class DeleteView(LoginRequiredMixin, generic.edit.DeleteView):
+    model = Article
+    success_url = reverse_lazy('bbs:index')
 
 class IndexView(generic.ListView):
-    model = Article
+    def get_queryset(self):
+        q_word = self.request.GET.get('query')
+ 
+        # チェックボックスにチェックが入っている項目だけを検索対象とする
+        selected_title = self.request.GET.get('title')
+        selected_article = self.request.GET.get('article')
+ 
+        if q_word:
+            if selected_title and selected_article:
+                object_list = Article.objects.filter(
+                    Q(title__icontains=q_word) | Q(content__icontains=q_word))
+            elif selected_title:
+                object_list = Article.objects.filter(Q(title__icontains=q_word))
+            else: # 投稿内容のみ、または両方ともチェックされていない場合は投稿内容のみを検索する
+                object_list = Article.objects.filter(Q(content__icontains=q_word))
+        else:
+            object_list = Article.objects.all()
+ 
+        return object_list
  
 class DetailView(generic.DetailView):
     model = Article
+
+class CreateView(LoginRequiredMixin, generic.edit.CreateView):
+    model = Article
+    fields = ['content', 'title', ]
+ 
+    def form_valid(self, form):
+        form.instance.author = self.request.user
+        return super(CreateView, self).form_valid(form)
